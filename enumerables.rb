8ed3b3e8e6d7c2c5dc6cc2d6125d9c49861a721f
OdongAlican <sandieo.2020@gmail.com>
2020-04-17 02:00:39 +0300

    added my_any and my_none methods

--- enumerables.rb
+++ enumerables.rb
@@ -37,6 +37,29 @@ module Enumerable
           false
         end
       end
+
+      def my_any?
+        if block_given?
+          values = false
+          my_each { |val| values = true if yield(val) }
+          values
+        else
+          true
+        end
+      end
+    
+      def my_none?
+        if block_given?
+          values = true
+          my_each { |val| values = false if yield(val) }
+          values
+        elsif self[0].nil? && !include?(true)
+          return true
+        else
+          return false
+        end
+        values
+      end
   end
   
 # Testing my_each method
@@ -57,4 +80,16 @@ module Enumerable
 # puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
 # puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
 # puts [nil, true, 99].my_all?                              #=> false
+
+# Testing my_any? method
+# puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+# puts [nil, true, 99].my_any?                              #=> true
+
+# Testing my_none? method
+# puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+# puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+# puts [].my_none?                                           #=> true
+# puts [nil].my_none?                                        #=> true
+# puts [nil, false].my_none?                                 #=> true
   
