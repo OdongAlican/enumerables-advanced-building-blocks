6ace481eb8278defb8106609c021f6af36e7d0cc
OdongAlican <sandieo.2020@gmail.com>
2020-05-06 20:58:01 +0300

    adds tests for my_select and my_all methods

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -22,4 +22,31 @@ describe Enumerable do
       expect(arr.my_each_with_index).to be_an Enumerator
     end
   end
+  describe '#my_select' do
+    it 'returns array elements for which the given block returns a true value' do
+      expect { |b| arr.my_select(&b) }.to yield_control
+    end
+
+    it 'returns an enumerator if no block is given' do
+      expect(arr.my_select).to be_an Enumerator
+    end
+  end
+
+  describe '#my_all?' do
+    it 'returns true if the block never returns false ' do
+      expect { |b| arr.my_all?(&b) }.to yield_control
+    end
+
+    it 'returns true if all values match as Integers' do
+      expect(arr.my_all?(Integer)).to be true
+    end
+    
+    it 'returns true if all values match the regexp' do
+      expect(word.my_all?(/d/)).to be true
+    end
+
+    it 'returns true when none of the collection members is false' do
+      expect(arr.my_all?).to be true
+    end
+  end
 end
